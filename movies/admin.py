06322df0b5a1854223383bad875d7c40e9e69209
Richard Kefa mcharo <59644856+richardkefa@users.noreply.github.com>
2023-04-14 08:46:59 +0000

    add movies, favorite user  movie models registered it to the data base run migrations

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import favoriteUserGenre,Movies
 
-# Register your models here.
+admin.site.register(favoriteUserGenre)
+admin.site.register(Movies)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+ 
+class Movies(models.Model):
+  title = models.CharField(max_length=200)
+  genre = models.CharField(max_length=200)
+  rating = models.FloatField()
+  description = models.TextField()
+  api_id = models.IntegerField()
 
-# Create your models here.
+  def __str__(self):
+    return self.title
+    
+class favoriteUserGenre(models.Model):
+  user = models.ForeignKey(User,on_delete = models.CASCADE,blank=True,null=True)
+  genre = models.CharField(max_length=200,blank=True) 
+
+  def __str__(self):
+    return self.genre
